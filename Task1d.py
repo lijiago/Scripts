c26585c9b8d968d0d6cc6fc5cbef5b045519b035
lijiago <lijia.go@duke.edu>
2023-10-30 11:37:56 -0400

    1d and 1e commit

---
+++ Task1d.py
@@ -0,0 +1,26 @@
+##---------------------------------------------------------------------
+## Task1d.py
+##
+## Description: Adding auto-generated output names
+##
+##
+## Created: Fall 2023
+## Author: lijia.go@duke.edu (for ENV859)
+##---------------------------------------------------------------------
+
+# import system modules
+import arcpy
+
+# Set workspace
+arcpy.env.workspace = "V:/ENV859_PS4/Data"
+arcpy.env.overwriteOutput = True 
+
+# Set local variables 
+in_features = "streams.shp"
+buffDist = sys.argv[1]
+out_feature_class = (f"V:/ENV859_PS4/Scratch/StrmBuff{int(buffDist)}m.shp")
+
+# Buffer the results
+arcpy.Buffer_analysis(in_features, out_feature_class, buffDist,'FULL','ROUND','ALL')
+
+print(arcpy.GetMessages)
