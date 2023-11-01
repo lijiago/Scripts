3cbe702cda19585d01531f4858bbf946724ef2d4
lijiago <lijia.go@duke.edu>
2023-11-01 11:17:23 -0400

    completed task 3

--- Task3.py
+++ Task3.py
@@ -10,6 +10,8 @@
 
 # import system modules
 import arcpy
+import os
+import sys
 
 # Set workspace
 arcpy.env.workspace = "V:/ENV859_PS4/Data"
@@ -19,29 +21,23 @@ arcpy.env.overwriteOutput = True
 TriCounties_fc = "V:\\ENV859_PS4\\Data\\TriCounties.shp"
 
 # creating a list of all 5 features that start with "BMR" in its name
-point_fc = arcpy.ListFeatureClasses(wild_card = 'BMR*')
+point_fc_shp = arcpy.ListFeatureClasses(wild_card = 'BMR*')
+#removing .shp in list
+point_fc = [item.replace('.shp', '') for item in point_fc_shp]
 
-# Checking if product edition
+# Checking if product edition is available 
 if arcpy.CheckProduct("ArcInfo") == "Available":
+    # conducting split analysis for items in list using a for loop
+    # saving output to specific folder 
     for i in point_fc:
         inFeatures = i
         splitFeatures = TriCounties_fc
         splitField = "CO_NAME"
-        newfolders = arcpy.CreateFolder_management("V:\\ENV859_PS4\\Scratch", f'{i}')
-        folderlist = newfolders
-        arcpy.Split_analysis(in_features = inFeatures, split_features = splitFeatures, split_field = splitField, out_workspace = newfolders[i])
+        newfolders = arcpy.CreateFolder_management("V:\\ENV859_PS4\\Scratch", f"{i}")
+        arcpy.Split_analysis(in_features = inFeatures, split_features = splitFeatures, split_field = splitField, out_workspace = newfolders)
 else:
     msg = 'ArcGIS for Desktop Advanced license not available'
     print(msg)
     sys.exit(msg)
 
-data_folder = r'W:\859_data\triangle'
 
-data_list = ["streams.shp", "stream_types.csv", "naip_imagery.tif"]
-
-user_item = "roads.shp"
-
-data_list.append(user_item)
-
-for N in data_list: 
-    print(data_folder  + "\\" + N)
